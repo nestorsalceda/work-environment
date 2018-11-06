11b189a9e702341d176dd607201aafbd5b02b922
Néstor Salceda <nestor.salceda@sysdig.com>
2018-11-06 09:58:28 +0100

    Fix EKS stuff

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -6,7 +6,7 @@
 #
 
 resource "aws_iam_role" "demo-cluster" {
-  name = "terraform-eks-demo-cluster"
+  name = "${var.cluster-name}-demo-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -35,7 +35,8 @@ resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" 
 }
 
 resource "aws_security_group" "demo-cluster" {
-  name        = "terraform-eks-demo-cluster"
+  name = "${var.cluster-name}-demo-cluster"
+
   description = "Cluster communication with worker nodes"
   vpc_id      = "${aws_vpc.demo.id}"
 
@@ -47,7 +48,7 @@ resource "aws_security_group" "demo-cluster" {
   }
 
   tags {
-    Name = "terraform-eks-demo"
+    Name = "${var.cluster-name}"
   }
 }
 
--- eks/variables.tf
+++ eks/variables.tf
@@ -3,6 +3,6 @@
 #
 
 variable "cluster-name" {
-  default = "terraform-eks-demo"
+  default = "sysdig-eks"
   type    = "string"
 }
