b6bc7b13134487f0fc0416e5b77c30bd3cd79698
Néstor Salceda <nestor.salceda@sysdig.com>
2019-03-13 12:49:57 +0100

    Add settings which allows to use more HP

--- gke-k8s/gke.tf
+++ gke-k8s/gke.tf
@@ -7,6 +7,9 @@ resource "google_container_cluster" "sysdig_work" {
   node_config {
     image_type = "ubuntu"
 
+    # Uncomment next line if you need more HP
+    # machine_type = "n1-standard-2"
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
